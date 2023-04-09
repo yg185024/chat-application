50e8289128a77c952106d5d4f82601777590413c
yg185024 <yg185024@ncr.com>
2023-04-09 12:48:16 +0530

    Added time stamp

--- src/script.js
+++ src/script.js
@@ -53,19 +53,36 @@ function sendMessage() {
     return;
   }
 
+  // Create a div element to hold the message
+  const messageDiv = document.createElement("div");
+  messageDiv.classList.add("sender-message-div");
+
   if( windowClient !== clientId){
     const owner = document.createElement("p");
     owner.innerText = clientId;
     owner.classList.add("sender-name");
-    chatHistory.appendChild(owner);
-    chatHistory.scrollTop = chatHistory.scrollHeight; // auto-scroll to the bottom of the chat container
+    messageDiv.appendChild(owner);
     changeTheValueOfWindowClient(clientId);
   }
 
-  const sentMessage = document.createElement("p");
+  // Create a span element to hold the message text
+  const sentMessage = document.createElement("span");
+  sentMessage.classList.add("sender-message");
   sentMessage.innerText = messageText;
-  sentMessage.classList.add("sent-message");
-  chatHistory.appendChild(sentMessage);
+  
+  // Create a span element to hold the timestamp
+  const timestamp = document.createElement("span");
+  timestamp.classList.add("sender-timestamp");
+  timestamp.innerText = getCurrentTime();
+
+  // Append the sentmessage text and timestamp to the message div
+  messageDiv.appendChild(sentMessage);
+  messageDiv.appendChild(timestamp);
+
+  // Append the message div to the chat box
+  chatHistory.appendChild(messageDiv);
+
+
   chatHistory.scrollTop = chatHistory.scrollHeight;
   messageInput.value = "";
 
@@ -75,19 +92,39 @@ function sendMessage() {
 
 // Function to receive msgs
 export function receiveMessage(member,msg){
+
+  // Create a div element to hold the message
+  const messageDiv = document.createElement("div");
+  messageDiv.classList.add("receiver-message-div");
+
   if( windowClient !== member){
     const owner = document.createElement("p");
     owner.innerText = member;
     owner.classList.add("receiver-name");
-    chatHistory.appendChild(owner);
+    owner.style.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    messageDiv.appendChild(owner);
+
     chatHistory.scrollTop = chatHistory.scrollHeight; 
     changeTheValueOfWindowClient(member);
   }
   
-  const receivedMessage = document.createElement("p");
+  // Create a span element to hold the message text
+  const receivedMessage = document.createElement("span");
+  receivedMessage.classList.add("receiver-message");
   receivedMessage.innerText = msg;
-  receivedMessage.classList.add("received-message");
-  chatHistory.appendChild(receivedMessage);
+  
+  // Create a span element to hold the timestamp
+  const timestamp = document.createElement("span");
+  timestamp.classList.add("receiver-timestamp");
+  timestamp.innerText = getCurrentTime();
+
+  // Append the sentmessage text and timestamp to the message div
+  messageDiv.appendChild(receivedMessage);
+  messageDiv.appendChild(timestamp);
+
+  // Append the message div to the chat box
+  chatHistory.appendChild(messageDiv);
+  
   chatHistory.scrollTop = chatHistory.scrollHeight;
   console.log(`${member} : ${msg}`);
 }
@@ -108,3 +145,13 @@ export function defaultMessage(clientId,action){
   console.log(`${ (clientId===0?"":clientId) } ${action}`);
 }  
 
+
+// Function to get the current time in HH:MM format
+function getCurrentTime() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const formattedHours = hours < 10 ? "0" + hours : hours;
+  const formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
+  return `${formattedHours}:${formattedMinutes}`;
+}
